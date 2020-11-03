ba3fe87ba6e09170ecc41552b93ac2f8f0e3c9f2
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2020-11-03 21:37:59 +0100

    Shortens the link to speedrun.com

--- commands/speedrun.js
+++ commands/speedrun.js
@@ -1,7 +1,7 @@
 import Command from "../command.js";
 export default class SpeedrunCommand extends Command {
 	async execute(message, parameters) {
-		await message.channel.send("You can check and watch the latest speedruns there:\nhttps://www.speedrun.com/super_bear_adventure");
+		await message.channel.send("You can check and watch the latest speedruns there:\nhttps://www.speedrun.com/sba");
 	}
 	async describe(message, command) {
 		return `Type \`${command}\` to know where to watch speedruns of the game`;
