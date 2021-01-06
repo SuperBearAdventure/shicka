60fdaeb9a3a48e85a98c0effd86f97492c0b5257
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2021-01-06 01:50:01 +0100

    Adds support for individual levels in the record feed

--- feeds/record.js
+++ feeds/record.js
@@ -26,9 +26,9 @@ export default class RecordFeed extends Feed {
 	async execute(start, end) {
 		const messages = [];
 		try {
-			const categories = new Map();
+			const levels = Object.create(null);
 			loop: for (let i = 0;; i += 20) {
-				const response = await fetch(`https://www.speedrun.com/api/v1/runs?game=9d3rrxyd&status=verified&orderby=verify-date&direction=desc&embed=category.variables&offset=${i}&max=20`);
+				const response = await fetch(`https://www.speedrun.com/api/v1/runs?game=9d3rrxyd&status=verified&orderby=verify-date&direction=desc&embed=category.variables,level&offset=${i}&max=20`);
 				const {data, pagination} = await response.json();
 				if (pagination.size === 0) {
 					break;
@@ -41,60 +41,70 @@ export default class RecordFeed extends Feed {
 					if (date > end) {
 						continue;
 					}
-					if (level !== null) {
-						continue;
-					}
+					const levelData = level.data;
+					const levelId = !Array.isArray(levelData) ? `level/${levelData.id}` : "category";
+					const {categories} = levels[levelId] ?? (levels[levelId] = {
+						levelName: !Array.isArray(levelData) ? `${levelData.name}: ` : "",
+						categories: Object.create(null),
+					});
 					const categoryData = category.data;
 					if (categoryData.miscellaneous) {
 						continue;
 					}
 					const categoryId = categoryData.id;
-					if (!categories.has(categoryId)) {
-						categories.set(categoryId, {
-							categoryName: categoryData.name,
-							leaderboards: new Map(),
-							variables: categoryData.variables.data.filter((variable) => {
-								return variable["is-subcategory"] && variable.mandatory;
-							}),
-						});
-					}
-					const {leaderboards, variables} = categories.get(categoryId);
-					const leaderboardId = variables.map((variable) => {
+					const {leaderboards} = categories[categoryId] ?? (categories[categoryId] = {
+						categoryName: categoryData.name,
+						leaderboards: Object.create(null),
+					});
+					const leaderboardData = categoryData.variables.data.filter((variable) => {
+						if (!variable["is-subcategory"]) {
+							return false;
+						}
+						const variableScope = variable.scope;
+						return variableScope.type !== "single-level" || `level/${variableScope.level}` === levelId;
+					});
+					const leaderboardId = leaderboardData.map((variable) => {
 						const variableId = variable.id;
 						return `var-${variableId}=${values[variableId]}&`;
 					}).join("");
-					if (!leaderboards.has(leaderboardId)) {
-						leaderboards.set(leaderboardId, variables.map((variable) => {
+					leaderboards[leaderboardId] ?? (leaderboards[leaderboardId] = {
+						leaderboardName: leaderboardData.length ? ` - ${leaderboardData.map((variable) => {
 							return `${variable.values.values[values[variable.id]].label}`;
-						}).join(", "));
-					}
+						}).join(", ")}` : "",
+					});
 				}
 			}
-			for (const [categoryId, {categoryName, leaderboards}] of categories) {
-				for (const [leaderboardId, leaderboardName] of leaderboards) {
-					const response = await fetch(`https://www.speedrun.com/api/v1/leaderboards/9d3rrxyd/category/${categoryId}?${leaderboardId}status=verified&embed=players&top=1`);
-					const {data} = await response.json();
-					const {players, runs} = data;
-					if (!runs.length) {
-						continue;
+			for (const levelId in levels) {
+				const {levelName, categories} = levels[levelId];
+				for (const categoryId in categories) {
+					const {categoryName, leaderboards} = categories[categoryId];
+					for (const leaderboardId in leaderboards) {
+						const {leaderboardName} = leaderboards[leaderboardId];
+						const response = await fetch(`https://www.speedrun.com/api/v1/leaderboards/9d3rrxyd/${levelId}/${categoryId}?${leaderboardId}status=verified&embed=players&top=1`);
+						const {data} = await response.json();
+						const {players, runs} = data;
+						if (!runs.length) {
+							continue;
+						}
+						const {status, times, videos} = runs[0].run;
+						const date = Date.parse(status["verify-date"]);
+						if (date <= start || date > end) {
+							continue;
+						}
+						const player = players.data[0];
+						const flag = "location" in player ? `:flag_${player.location.country.code.toLowerCase()}: ` : "";
+						const name = "names" in player ? player.names.international : player.name;
+						const user = `*${Util.escapeMarkdown(flag)}${Util.escapeMarkdown(name)}*`;
+						const {primary_t} = times;
+						const minutes = `${primary_t / 60 | 0}`.padStart(2, "0");
+						const seconds = `${primary_t % 60 | 0}`.padStart(2, "0");
+						const centiseconds = `${primary_t * 100 % 100 | 0}`.padStart(2, "0");
+						const time = `**${Util.escapeMarkdown(`${minutes}:${seconds}.${centiseconds}`)}**`;
+						const category = `*${Util.escapeMarkdown(`${levelName}${categoryName}${leaderboardName}`)}*`;
+						const links = "links" in videos ? videos.links : [];
+						const video = links.length ? `\n${links[0].uri}` : "";
+						messages.push(`${user} set a new world record in the ${category} category: ${time}!${video}`);
 					}
-					const {status, times, videos} = runs[0].run;
-					const date = Date.parse(status["verify-date"]);
-					if (date <= start || date > end) {
-						continue;
-					}
-					const player = players.data[0];
-					const flag = player.location.country.code.toLowerCase();
-					const name = player.names.international;
-					const user = `*:flag_${Util.escapeMarkdown(flag)}: ${Util.escapeMarkdown(name)}*`;
-					const {primary_t} = times;
-					const minutes = `${primary_t / 60 | 0}`.padStart(2, "0");
-					const seconds = `${primary_t % 60 | 0}`.padStart(2, "0");
-					const centiseconds = `${primary_t * 100 % 100 | 0}`.padStart(2, "0");
-					const time = `**${Util.escapeMarkdown(`${minutes}:${seconds}.${centiseconds}`)}**`;
-					const category = `*${Util.escapeMarkdown(`${categoryName}${leaderboardName && ` - ${leaderboardName}`}`)}*`;
-					const video = videos.links[0].uri;
-					messages.push(`${user} set a new world record in the ${category} category: ${time}!\n${video}`);
 				}
 			}
 		} catch (error) {
