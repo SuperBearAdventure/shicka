937bb1745058588b21eb1558ab20a7a299c4099a
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2021-02-03 00:31:56 +0100

    Adds support for category extensions in the record feed

--- commands/speedrun.js
+++ commands/speedrun.js
@@ -1,17 +1,19 @@
 import Command from "../command.js";
 const leaderboards = [
-	"*Full-game*, *Missions*, *Race*: https://www.speedrun.com/sba",
+	"*Full-game*: https://www.speedrun.com/sba, https://www.speedrun.com/sbace",
 	"*Turtle Village*: https://www.speedrun.com/sba/Turtle_Village",
 	"*Snow Valley*: https://www.speedrun.com/sba/Snow_Valley",
 	"*Beemothep Desert*: https://www.speedrun.com/sba/Beemothep_Desert",
 	"*Giant House*: https://www.speedrun.com/sba/Giant_House",
+	"*Missions*: https://www.speedrun.com/sbace/Missions",
+	"*Races*: https://www.speedrun.com/sbace/Races",
 ];
 export default class SpeedrunCommand extends Command {
 	async execute(message, parameters) {
 		const links = leaderboards.map((leaderboard) => {
 			return `- ${leaderboard}`;
 		}).join("\n");
-		await message.channel.send(`You can check and watch the latest speedruns there:\n${links}`);
+		await (await message.channel.send(`You can check and watch the latest speedruns there:\n${links}`)).suppressEmbeds();
 	}
 	async describe(message, command) {
 		return `Type \`${command}\` to know where to watch speedruns of the game`;
