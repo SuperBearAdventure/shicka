7d4bb59e7ab9e434a8a84397185ffb7e0ee758c1
Tristan LE GODAIS <36267812+PolariTOON@users.noreply.github.com>
2021-05-21 15:02:28 +0200

    Fixes the `?shop` command alias

--- commands/shop.js
+++ commands/shop.js
@@ -2,7 +2,7 @@ import Command from "../command.js";
 export default class ShopCommand extends Command {
 	async execute(message, parameters) {
 		const {commands, prefix} = message.client;
-		commands.outfit.execute(message, ["shop", parameters.slice(1)]);
+		await commands.outfit.execute(message, ["shop", ...parameters.slice(1)]);
 		const search = parameters.slice(1).join(" ") && " Some outfit";
 		await message.channel.send(`> By the way, \`${prefix}shop${search}\` is deprecated and will be removed soon, please use \`${prefix}outfit${search}\` instead.`);
 	}
