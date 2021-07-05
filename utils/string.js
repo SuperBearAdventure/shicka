e2c62faa110295a9e5110071d77dd2d7dfccba62
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2021-07-06 00:41:41 +0200

    Makes the fuzzy search filter out candidates that are not supersequences of the search string
    Also makes the fuzzy search on top of the LCS distance,
    which does not allow substitutions, contrary to the Levenshtein distance

--- utils/string.js
+++ utils/string.js
@@ -1,4 +1,4 @@
-function levenshtein(a, b) {
+function editDistance(a, b, substitution) {
 	if (a.length < b.length) {
 		[a, b] = [b, a];
 	}
@@ -9,20 +9,44 @@ function levenshtein(a, b) {
 	}
 	const costs = Array.from({length: lj}, (value, key) => (key));
 	for (let i = 0; i < li; ++i) {
+		const item = a[i];
 		let extracted = i - 1;
 		let inserted = i;
 		for (let j = 0; j < lj; ++j) {
 			const substituted = extracted;
 			extracted = costs[j];
-			inserted = a[i] !== b[j] ? Math.min(substituted, extracted, inserted) + 1 : substituted;
+			inserted = item !== b[j] ? Math.min(substitution ? substituted : Number.POSITIVE_INFINITY, extracted, inserted) + 1 : substituted;
 			costs[j] = inserted;
 		}
 	}
 	return costs[lj - 1] + 1;
 }
+function isSubsequence(needle, haystack) {
+	const li = needle.length;
+	const lj = haystack.length;
+	if (lj < li) {
+		return false;
+	}
+	let i = 0;
+	let j = 0;
+	loop: while (i < li) {
+		const item = needle[i++];
+		while (j < lj) {
+			if (item === haystack[j++]) {
+				continue loop;
+			}
+		}
+		return false;
+	}
+	return true;
+}
 export function nearest(search, array, stringify) {
 	const {value} = array.reduce((accumulator, value) => {
-		const distance = levenshtein(search, stringify(value));
+		const string = stringify(value);
+		if (!isSubsequence(search, string)) {
+			return accumulator;
+		}
+		const distance = editDistance(search, string, false);
 		if (distance < accumulator.distance) {
 			return {value, distance};
 		}
