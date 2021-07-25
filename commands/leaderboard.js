12a9c14da06270499eda4eacc321af17d7faf980
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2021-07-25 17:03:22 +0200

    Renames the `?speedrun` command into `?leaderboard`

---
+++ commands/leaderboard.js
@@ -0,0 +1,22 @@
+import Command from "../command.js";
+const leaderboards = [
+	"*Full-game leaderboard*: https://www.speedrun.com/sba",
+	"*Turtle Village leaderboard*: https://www.speedrun.com/sba/Turtle_Village",
+	"*Snow Valley leaderboard*: https://www.speedrun.com/sba/Snow_Valley",
+	"*Beemothep Desert leaderboard*: https://www.speedrun.com/sba/Beemothep_Desert",
+	"*Giant House leaderboard*: https://www.speedrun.com/sba/Giant_House",
+	"*Missions leaderboard*: https://www.speedrun.com/sbace/Missions",
+	"*Races leaderboard*: https://www.speedrun.com/sbace/Races",
+	"*Category Extensions*: https://www.speedrun.com/sbace",
+];
+export default class LeaderboardCommand extends Command {
+	async execute(message, parameters) {
+		const links = leaderboards.map((leaderboard) => {
+			return `- ${leaderboard}`;
+		}).join("\n");
+		await (await message.channel.send(`You can watch community speedruns there:\n${links}`)).suppressEmbeds(true);
+	}
+	async describe(message, command) {
+		return `Type \`${command}\` to know where to watch community speedruns of the game`;
+	}
+}
