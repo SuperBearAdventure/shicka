dc4718b53c4fb9361e374c09696279306f1a8512
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2021-07-25 16:50:37 +0200

    Adds the `?store` command

---
+++ commands/store.js
@@ -0,0 +1,16 @@
+import Command from "../command.js";
+const stores = [
+	"*European store*: https://shop.spreadshirt.net/SuperBearAdventure",
+	"*American and Oceanian store*: https://shop.spreadshirt.com/SuperBearAdventure",
+];
+export default class StoreCommand extends Command {
+	async execute(message, parameters) {
+		const links = stores.map((store) => {
+			return `- ${store}`;
+		}).join("\n");
+		await (await message.channel.send(`You can buy official products of the game there:\n${links}`)).suppressEmbeds(true);
+	}
+	async describe(message, command) {
+		return `Type \`${command}\` to know where to buy offical products of the game`;
+	}
+}
