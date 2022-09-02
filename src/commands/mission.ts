7b03915c44c40c814b6dee164f4b69a30eb60a3b
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2022-09-03 01:04:51 +0200

    Reverts "Reverts "Constrains choices on autocomplete options""
    This now seems to work fine on every platform.

--- src/commands/mission.ts
+++ src/commands/mission.ts
@@ -1,6 +1,7 @@
 import type {
 	ApplicationCommandData,
 	ApplicationCommandOptionChoiceData,
+	ApplicationCommandOptionData,
 	AutocompleteFocusedOption,
 	AutocompleteInteraction,
 	CommandInteraction,
@@ -44,12 +45,14 @@ const missionCommand: Command = {
 			name: commandName,
 			description: commandDescription,
 			options: [
-				{
-					type: "STRING",
+				((): ApplicationCommandOptionData & {minValue: number, maxValue: number} => ({
+					type: "INTEGER",
 					name: missionOptionName,
 					description: missionOptionDescription,
+					minValue: 0,
+					maxValue: missions.length - 1,
 					autocomplete: true,
-				},
+				}))(),
 			],
 		};
 	},
@@ -66,10 +69,11 @@ const missionCommand: Command = {
 				return name.toLowerCase();
 			});
 			const suggestions: ApplicationCommandOptionChoiceData[] = results.map((mission: Mission): ApplicationCommandOptionChoiceData => {
+				const {id}: Mission = mission;
 				const name: string = `${challenges[mission.challenge].name["en-US"]} in ${levels[mission.level].name["en-US"]}`;
 				return {
 					name: name,
-					value: name,
+					value: id,
 				};
 			});
 			await interaction.respond(suggestions);
@@ -81,8 +85,8 @@ const missionCommand: Command = {
 		const {options}: CommandInteraction = interaction;
 		const missionCount: number = missions.length;
 		const now: number = Math.floor((interaction.createdTimestamp + 7200000) / 86400000);
-		const search: string | null = options.getString(missionOptionName);
-		if (search == null) {
+		const id: number | null = options.getInteger(missionOptionName);
+		if (id == null) {
 		const schedules: string[] = [];
 		for (let k: number = -1; k < 2; ++k) {
 			const day: number = now + k;
@@ -99,18 +103,7 @@ const missionCommand: Command = {
 		});
 		return;
 		}
-		const results: Mission[] = nearest<Mission>(search.toLowerCase(), missions, 1, (mission: Mission): string => {
-			const name: string = `${challenges[mission.challenge].name["en-US"]} in ${levels[mission.level].name["en-US"]}`;
-			return name.toLowerCase();
-		});
-		if (results.length === 0) {
-			await interaction.reply({
-				content: `I do not know any mission with this name.`,
-				ephemeral: true,
-			});
-			return;
-		}
-		const mission: Mission = results[0];
+		const mission: Mission = missions[id];
 		const schedules: string[] = [];
 		for (let k: number = -1; k < 2 || schedules.length < 2; ++k) {
 			const day: number = now + k;
