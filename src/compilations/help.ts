a1f02b09b51c1f0e276459827be76b76fb563e99
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2024-05-21 00:04:31 +0200

    Adds a `feature` option for the `/help` command to get a description of a given feature

--- src/compilations/help.ts
+++ src/compilations/help.ts
@@ -4,14 +4,22 @@ import {help} from "../definitions.js";
 import {compileAll} from "../utils/string.js";
 type HelpLocalizations = Localized<(groups: Help["help"]) => string>;
 type ReplyLocalizations = Localized<(groups: Help["reply"]) => string>;
+type BareReplyLocalizations = Localized<(groups: Help["bareReply"]) => string>;
+type NoFeatureReplyLocalizations = Localized<(groups: Help["noFeatureReply"]) => string>;
 type HelpCompilation = {
 	help: HelpLocalizations,
 	reply: ReplyLocalizations,
+	bareReply: BareReplyLocalizations,
+	noFeatureReply: NoFeatureReplyLocalizations,
 };
 const helpLocalizations: HelpLocalizations = compileAll<Help["help"]>(help["help"]);
 const replyLocalizations: ReplyLocalizations = compileAll<Help["reply"]>(help["reply"]);
+const bareReplyLocalizations: BareReplyLocalizations = compileAll<Help["bareReply"]>(help["bareReply"]);
+const noFeatureReplyLocalizations: NoFeatureReplyLocalizations = compileAll<Help["noFeatureReply"]>(help["noFeatureReply"]);
 const helpCompilation: HelpCompilation = {
 	help: helpLocalizations,
 	reply: replyLocalizations,
+	bareReply: bareReplyLocalizations,
+	noFeatureReply: noFeatureReplyLocalizations,
 };
 export default helpCompilation;
--- src/dependencies/help.d.ts
+++ src/dependencies/help.d.ts
@@ -1,12 +1,22 @@
 type HelpGroups = {
 	commandMention: () => string,
+	featureOptionDescription: () => string,
 };
 type ReplyGroups = {
 	memberMention: () => string,
 	featureList: () => string,
 };
+type BareReplyGroups = {
+	memberMention: () => string,
+	featureList: () => string,
+};
+type NoFeatureReplyGroups = {
+	memberMention: () => string,
+};
 type HelpDependency = {
 	help: HelpGroups,
 	reply: ReplyGroups,
+	bareReply: BareReplyGroups,
+	noFeatureReply: NoFeatureReplyGroups,
 };
 export type {HelpDependency as default};
